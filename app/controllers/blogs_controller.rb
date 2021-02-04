fb3acf76731982f7fd778ccbe79f0fa41a3c14f0
hitoshi-suito <htshts153@gmail.com>
2021-02-04 10:39:02 +0900

    ブログ削除機能追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,6 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+
   def index
     @blogs = Blog.all
   end
@@ -17,15 +19,12 @@ class BlogsController < ApplicationController
   end
 
   def show
-    @blog = Blog.find(params[:id])
   end
 
   def edit
-    @blog = Blog.find(params[:id])
   end
 
   def update
-    @blog = Blog.find(params[:id])
     if @blog.update(blog_params)
       redirect_to blogs_path, notice:"ブログを編集しました！"
     else
@@ -33,8 +32,17 @@ class BlogsController < ApplicationController
     end
   end
 
+  def destroy
+    @blog.destroy
+    redirect_to blogs_path, notice:"ブログを削除しました！"
+  end
+
   private
   def blog_params
     params.require(:blog).permit(:content)
+  end
+
+  def set_blog
+    @blog = Blog.find(params[:id])
   end
 end
